5396fb2465d07ba2eaa6db0166700dbab60a0924
S-Shimotori <490.frostybirds@gmail.com>
2017-02-06 01:12:26 +0900

    for linux

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "APIKit",
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
+        .Package(url: "https://github.com/S-Shimotori/Result.git", Version(3, 1, 1)),
     ],
     exclude: ["Sources/APIKit/BodyParameters/AbstractInputStream.m"]
 )
