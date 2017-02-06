d963b99a1382eda471bdce56b6eb9ee21a672075
S-Shimotori <490.frostybirds@gmail.com>
2017-02-07 00:08:54 +0900

    Modify branch

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "APIKit",
     dependencies: [
-        .Package(url: "https://github.com/S-Shimotori/Result.git", Version(3, 1, 1)),
+        .Package(url: "https://github.com/S-Shimotori/Result.git", Version(3, 1, 2)),
     ],
     exclude: ["Sources/APIKit/BodyParameters/AbstractInputStream.m"]
 )
